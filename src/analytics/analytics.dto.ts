f192b33ef30558dda9cb8b00c730ad68350f2929
HM <hieumdd@gmail.com>
2023-01-16 23:01:25 +0700

    fix: Relax GlobalFilterQueryDto

--- src/analytics/analytics.dto.ts
+++ src/analytics/analytics.dto.ts
@@ -1,11 +1,11 @@
 export class GlobalFilterQueryDto {
-    companyName: string;
-    div: string;
-    hq: string;
-    reg: string;
-    st: string;
-    symbol: string;
-    type: string;
+    companyName?: string;
+    div?: string;
+    hq?: string;
+    reg?: string;
+    st?: string;
+    symbol?: string;
+    type?: string;
 }
 
 export class DateFilterQueryDto {
