82d6f7add4023c42594b5d874a71b6d09c4e11d4
HM <hieumdd@gmail.com>
2023-02-15 20:17:56 +0700

    feat: Add cppColumn query

--- src/analytics/social-profile/social-profile.controller..ts
+++ src/analytics/social-profile/social-profile.controller..ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { AnalyticsService } from '../analytics.service';
-import { CompanyFilterQuery, GlobalFilterQuery, RankFilterQuery } from '../analytics.dto';
+import { CompanyFilterQuery, CppFilterQuery, GlobalFilterQuery, RankFilterQuery } from '../analytics.dto';
 
 const route = 'social-profile';
 
@@ -27,11 +27,13 @@ export class SocialProfileController {
     @Get('loser-profile')
     async loserProfile(
         @Query() companyFilter: CompanyFilterQuery,
+        @Query() cppFilter: CppFilterQuery,
         @Query() globalFilter: GlobalFilterQuery,
         @Query() rankFilter: RankFilterQuery,
     ) {
         return this.analyticsService.query(`${route}/loser-profile`, {
             ...companyFilter,
+            ...cppFilter,
             ...globalFilter,
             ...rankFilter,
         });
