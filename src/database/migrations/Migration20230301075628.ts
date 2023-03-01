bafe1d4244af2f82617938443926961bd175c50a
HM <hieumdd@gmail.com>
2023-03-01 15:05:59 +0700

    misc: Format

--- src/database/migrations/Migration20230301075628.ts
+++ src/database/migrations/Migration20230301075628.ts
@@ -1,14 +1,14 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230301075628 extends Migration {
+    async up(): Promise<void> {
+        this.addSql(
+            'alter table "user" add column "first_name" varchar(255) not null, add column "last_name" varchar(255) not null;',
+        );
+    }
 
-  async up(): Promise<void> {
-    this.addSql('alter table "user" add column "first_name" varchar(255) not null, add column "last_name" varchar(255) not null;');
-  }
-
-  async down(): Promise<void> {
-    this.addSql('alter table "user" drop column "first_name";');
-    this.addSql('alter table "user" drop column "last_name";');
-  }
-
+    async down(): Promise<void> {
+        this.addSql('alter table "user" drop column "first_name";');
+        this.addSql('alter table "user" drop column "last_name";');
+    }
 }
